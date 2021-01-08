cdda54ca677f6fda05c83acf53c1019f266ae40f
Dany Laporte <dany_laporte@hotmail.com>
2021-01-08 09:54:49 -0500

    Initial Commit

---
+++ src/sync.rs
@@ -0,0 +1,66 @@
+use once_cell::sync::OnceCell;
+use std::future::Future;
+use tokio::sync::Mutex;
+
+pub struct AsyncOnceCell<T> {
+    cell: OnceCell<T>,
+    lock: Mutex<()>,
+}
+
+impl<T> AsyncOnceCell<T> {
+    pub fn get(&self) -> Option<&T> {
+        self.cell.get()
+    }
+
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        self.cell.get_mut()
+    }
+
+    pub async fn get_or_init<F>(&self, f: F) -> &T
+    where
+        F: Future<Output = T>,
+    {
+        if let Some(v) = self.cell.get() {
+            return v;
+        }
+
+        let _ = self.lock.lock().await;
+
+        if let Some(v) = self.cell.get() {
+            return v;
+        }
+
+        let v = f.await;
+        self.cell.get_or_init(|| v)
+    }
+
+    pub async fn get_or_try_init<F, E>(&self, f: F) -> Result<&T, E>
+    where
+        F: Future<Output = Result<T, E>>,
+    {
+        if let Some(v) = self.cell.get() {
+            return Ok(v);
+        }
+
+        let _ = self.lock.lock().await;
+
+        if let Some(v) = self.cell.get() {
+            return Ok(v);
+        }
+
+        let r = f.await;
+        self.cell.get_or_try_init(|| r)
+    }
+
+    pub fn into_inner(self) -> Option<T> {
+        self.cell.into_inner()
+    }
+
+    pub fn set(&self, value: T) -> Result<(), T> {
+        self.cell.set(value)
+    }
+
+    pub fn take(&mut self) -> Option<T> {
+        self.cell.take()
+    }
+}
